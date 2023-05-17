d8b70c3ea557b8fd34357113dad8896c9072e23d
rstreep <127055958+rstreep@users.noreply.github.com>
2023-05-16 23:05:26 -0400

    started init function and completed writefile function

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
+const writeFiles = require('./utils/generateMarkdown');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function generateReadme(fileName, data) {
+    fs.writeFile('README1.md', data, (err) =>
+    err ? console.error(err) : console.log('Success!')
+    );
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+// function will load a menu which will have questions to help write the read.me
+function init() {
+    inquirer
+    .createPromptModule([
+        {
+            X
+        }
+    ])
+    .then( answers => {
+        console.log(answers);
+        generateReadme(writeFile)
+    })
+}
 
 // Function call to initialize app
 init();
